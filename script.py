16bc7c3ddfff3a9d6d114c8c19d99b07e84802b9
AdegiteA <88291164+AdegiteA@users.noreply.github.com>
2021-08-04 18:54:02 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,1 @@
-name="Adegite Adejuwon"
-email="adegiteadejuwon@gmail.com"
-slackusername="@Juwon"
-biostack="Genomics"
-print(name)
-print(email)
-print(slackusername)
-print(biostack)
+print("Adegite Adejuwon\nadegiteadejuwon@gmail.com\n@juwon\nVaccine development")
