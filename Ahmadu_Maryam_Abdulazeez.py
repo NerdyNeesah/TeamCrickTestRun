56acad2ec2ff74349678db9dae338bb98ce26e2a
Ahmadu, Maryam Abdulazeez <abdulazeezmaryam108@gmail.com>
2021-08-05 12:47:15 +0100

    Ahmadu Maryam Abdulazeez script assignment

---
+++ Ahmadu_Maryam_Abdulazeez.py
@@ -0,0 +1,7 @@
+name = "Ahmadu Maryam Abdulazeez,"
+email_address ="abdulazeezmaryam108@gmail.com,"
+slack_username = "@maryam,"
+biostack = "genomics,"
+twitter_handle = "maryamahmadu108,"
+hamming_distance = 0
+print(name, email_address,slack_username,biostack,twitter_handle,hamming_distance)
