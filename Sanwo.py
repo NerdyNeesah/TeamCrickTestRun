46159c855dae3ab608eea7376ff76e18f59bf83a
Samuel Adesanwo <83287228+Sanwogram@users.noreply.github.com>
2021-08-06 19:27:09 +0100

    Add files via upload

---
+++ Sanwo.py
@@ -0,0 +1,5 @@
+def personal_details():
+    name, email, slack_username, biostack, twitter_handle, hamming_distance = "Samuel Adesanwo", "adesawosamuel0@gmail.com", "@Sanwogram", "Data Analytics", "@Sanwogram1", "0"
+    print("Name: {}\nEmail: {}\nSlack_username: {}\nBiostack: {}\nTwitter_handle: {}\nHamming_distance: {}".format(name, email, slack_username, biostack, twitter_handle, hamming_distance))
+
+personal_details()
